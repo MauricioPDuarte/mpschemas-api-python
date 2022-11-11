33485a13cf5d207b82d9792e06bd18fb9d84081a
Mauricio Pruss Duarte <mauricio.duarte@ambisis.com.br>
2022-11-11 10:01:49 -0300

    Config database

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask, make_response, jsonify, request
 from schemas import Schemas;
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
+
+# conexão com mysql
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Master123@localhost/mpschemas?charset=utf8mb4'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+
+api = Api(app)
+
+db = SQLAlchemy()
+db.init_app(app)
+
 
 @app.route("/schemas", methods=['GET'])
 def get_schemas():
@@ -23,3 +39,10 @@ def create_schema():
             data=schema
         ),
     )
+
+
+with app.app_context(): 
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
